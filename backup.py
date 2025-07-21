f2a451d671fbf7cdce97f54b4ff9a475a9f1f7dc
Vinicius Santos <151114473+ViniciusS4ntos@users.noreply.github.com>
2025-07-20 22:34:33 -0300

    Update backup.py

--- backup.py
+++ backup.py
@@ -1,13 +1,14 @@
 import shutil
 import time as t
-try:
-    origem = r"\\DESKTOP-VJ8B4AG\impressão digital\EXECELL\dados.xlsx"
-    destino = r"C:\Users\JESUS TE AMA\Desktop\backup"
-    shutil.copy(origem, destino)
-    print("Backup feito com sucesso!")
-except FileNotFoundError:
-    print("\nErro ao procurar o arquivo\n")
-except PermissionError:
-    print("\nSem permissão para acessar o arquivo ou pasta\n")
-except Exception as e:
-    print(f"\nErro inesperado: {e}\n")
+def Backup():
+    try:
+        origem = r"C:\Users\MIKE\Desktop\automatizacaoExcell-main\automatizacaoExcell-main\dados.xlsx"
+        destino = r"C:\Users\MIKE\Desktop"
+        shutil.copy(origem, destino)
+        print("Backup feito com sucesso!")
+    except FileNotFoundError:
+        print("\nErro ao procurar o arquivo\n")
+    except PermissionError:
+        print("\nSem permissão para acessar o arquivo ou pasta\n")
+    except Exception as e:
+        print(f"\nErro inesperado: {e}\n")
